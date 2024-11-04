aa501970e544d2101cc1ded5bf3cdc8e78a7e87a
anik12136 <anikbiswas431@gmail.com>
2024-11-04 17:53:11 +0600

    get data from mongodb database

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { text } = require('express');
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.00oqpy6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +22,30 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+        const demoCourses   =  client.db("ULearning").collection("demoCourses");
+        const formCourses   =  client.db("ULearning").collection("formCourses");
+        const users         =  client.db("ULearning").collection("users");
+        const courseRequest =  client.db("ULearning").collection("courseRequest");
+        const communityPost =  client.db("ULearning").collection("community");
+        const bookmarks     =  client.db("ULearning").collection("bookmarks");
+        const cartCollection=  client.db("ULearning").collection("carts");
+
+
+    // ----------APIs------------------------------------------------------------
+        // Demo course .....api
+        app.get('/demoCourses', async (req, res) => {
+            const result = await demoCourses.find().toArray();
+            res.send(result);
+        })
+
+
+     // -----------end APIs-----------------------------------------------------------
+
+
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
